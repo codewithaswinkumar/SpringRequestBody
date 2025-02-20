564e1b9a2a656bf4b338907bb1947723abe56dcb
codewithaswinkumar <aswinkumarch10@gmail.com>
2025-02-20 22:05:47 +0530

    Intial Commit

---
+++ src/main/java/com/jsp/controller/UserController.java
@@ -0,0 +1,27 @@
+package com.jsp.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.jsp.entitys.User;
+
+@RestController
+public class UserController
+{
+   @GetMapping("/print")
+   public String print()
+   {
+	   System.out.println("hii...done");
+	   return "success";
+   }
+//   @PostMapping("/insert")
+//   public User insert()
+//   {
+//	   User u = new User();
+//	   u.setName("aswin");
+//	   u.setAge(22);
+//	   u.setLoc("vizag");
+//	   	return u;	
+//   }
+}
---
+++ src/main/java/com/jsp/entitys/User.java
@@ -0,0 +1,27 @@
+//package com.jsp.entitys;
+//
+//import javax.persistence.GeneratedValue;
+//import javax.persistence.Id;
+//
+//import lombok.Getter;
+//import lombok.Setter;
+//import lombok.ToString;
+//@Setter
+//@Getter
+//@ToString
+//@Entity
+//@Table(name = "userfor")
+//public class User 
+//{
+//	@Id
+//	@GeneratedValue
+//	private int uid;
+// private String name;
+// private String loc;
+// private int age;
+// 
+//public User() {
+//	
+//}
+//
+//}
